b0eb711b1b278f1d7808f383a33a19d7aa878e5e
Лисёнок Живой <bleach-2001@mail.ru>
2024-04-04 20:09:45 +0300

    ...

---
+++ FileSystemHelper/NodeSeeker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemHelper
+{
+    public static class NodeSeeker
+    {
+        public static FSHandler FsHandler = new FSHandler();
+
+        public static int CheckedFolders = 0;
+        public static List<NodeEntity> FindByName(NodeEntity directory, string keyWord, Func<int, int> func)
+        {
+            var result = new List<NodeEntity>();
+            var children = FsHandler.GetNodeEntitiesChildren(directory.FullPath);
+
+            foreach (var child in children)
+            {
+                if (child.Name.ToUpper().Contains(keyWord.ToUpper()))
+                    result.Add(child);
+
+                if (child.Type == NodeType.Directory)
+                    result.AddRange(FindByName(child, keyWord, func));
+            }
+
+            CheckedFolders++;
+
+            func.Invoke(CheckedFolders);
+            return result;
+        }
+    }
+}
